3b3b28f8b76a5d1b9b464ef916bcc732ed339ddd
Santiago <srivas@itba.edu.ar>
2024-04-10 15:56:46 -0300

    Implemented real time graphing

--- main.py
+++ main.py
@@ -1,99 +1,34 @@
-from src.classes import Class
 from src.functions import print_points, Character
 from src.crossover import (single_point_crossover,
                            double_point_crossover,
                            ring_crossover,
                            uniform_crossover)
 from src.selection import (Population,
-                           select,
-                           elite_selection,
-                           roulette_selection,
-                           boltzmann_selection,
-                           universal_selection,
-                           tournament_prob,
-                           ranking,
-                           tournament_det)
-from src.mutation import mutate, multi_gene_mutation, gene_mutation
-from src.replacement import traditional_replacement, youth_favoured_replacement
+                           select
+                           )
+from src.mutation import mutate
 from src.items import POINTS_SUM
 from src.player import HEIGHT_MAX, HEIGHT_MIN
 from src.criteria import (State,
                           MaxGenerations,
                           MinFitness,
                           ContentCriteria)
+from src.config import (Config,
+                        population_class,
+                        selection_methods,
+                        crossover_methods,
+                        replacement_method,
+                        mutation_method
+                        )
 
 from heapq import heappush
 from collections import defaultdict
 
-import json
 import math
 import random
-
-
-class Config:
-    def __init__(self, config_file):
-        # Read the JSON configuration from file
-        with open(config_file, 'r') as f:
-            config = json.load(f)
-
-        # Assign configuration parameters to class attributes
-        self.population_size = config["population_size"]
-        self.child_rate = config["child_rate"]
-        self.mutation_rate = config["mutation_rate"]
-        self.mutation_delta = config["mutation_delta"]
-        self.mutation_method = config["mutation_method"]
-        self.gene_to_modify = config["gene_to_modify"]
-        self.uniform_mutation = config["uniform_mutation"]
-        self.max_generations = config["max_generations"]
-        self.a = config["A"]
-        self.selection_method_1 = config["selection_method_1"]
-        self.selection_method_2 = config["selection_method_2"]
-        self.b = config["B"]
-        self.replacement_method = config["replacement_method"]
-        self.replacement_selection_method_1 = config["replacement_selection_method_1"]
-        self.replacement_selection_method_2 = config["replacement_selection_method_2"]
-        self.crossover_method = config["crossover_method"]
-        self.boltzmann_temperature_start = config["boltzmann_temperature_start"]
-        self.boltzmann_temperature_end = config["boltzmann_temperature_end"]
-        self.boltzmann_temperature_constant = config["boltzmann_temperature_constant"]
-        self.end_criteria = config["end_criteria"]
-        self.max_generations = config["max_generations"]
-        self.content_criteria_limit = config["content_criteria_limit"]
-        self.content_criteria_delta = config["content_criteria_delta"]
-        self.end_delta = config["end_delta"]
-        self.min_fitness = config["min_fitness"]
-
-
-selection_methods = defaultdict(lambda: elite_selection)
-selection_methods.update({
-    'elite': elite_selection,
-    'roulette': roulette_selection,
-    'universal': universal_selection,  # TODO
-    'boltzmann': boltzmann_selection,
-    'tournament_det': tournament_det,
-    'tournament_prob': tournament_prob,
-    'ranking': ranking     # TODO
-})
-
-crossover_methods = defaultdict(lambda: single_point_crossover)
-crossover_methods.update({
-    'single_point': single_point_crossover,
-    'double_point': double_point_crossover,
-    'ring': ring_crossover,
-    'uniform': uniform_crossover
-})
-
-replacement_method = defaultdict(lambda: youth_favoured_replacement)
-replacement_method.update({
-    'youth_favoured': youth_favoured_replacement,
-    'traditional': traditional_replacement
-})
-
-mutation_method = defaultdict(lambda: gene_mutation)
-mutation_method.update({
-    'gene': gene_mutation,
-    'multi_gene': multi_gene_mutation
-})
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
 
 if __name__ == "__main__":
@@ -112,10 +47,50 @@ if __name__ == "__main__":
     state = State([], 0)
 
     for i in range(1, config.population_size):
-        character = Character.random(Class.ARCHER)
+        character = Character.random(population_class[config.population_class])
         heappush(generation_0, character)
 
     state.generations.append(generation_0)
+
+    plt.figure()
+    sns.set_style("whitegrid")
+
+    line, = plt.plot([], [], marker='o', linestyle='-')
+    line_individuals, = plt.plot(
+        [], [], marker='.', linestyle='', label='Individual Performance')
+    line_mean, = plt.plot(
+        [], [], marker='.', linestyle='', label='Mean Performance')
+    line_median, = plt.plot(
+        [], [], marker='.', linestyle='', label='Median Performance')
+    plt.xlabel('Generation')
+    plt.ylabel('Performance')
+
+    # Set the axes limits
+    plt.xlim(0, config.max_generations)
+    plt.ylim(0, 100)
+
+    best_performance_data = []
+    individual_performance_data = []
+    mean_performance_data = []
+    median_performance_data = []
+
+    def update_plot(gen_i, state):
+        best_of_last = max(state.generations[-1], key=lambda x: x.performance)
+        best_performance_data.append(best_of_last.performance)
+        line.set_data(range(1, gen_i + 1), best_performance_data)
+
+        individual_performances = [
+            individual.performance for individual in state.generations[-1]]
+        individual_performance_data.append(individual_performances)
+        # line_individuals.set_data(np.tile(range(1, gen_i + 1), (len(individual_performances), 1)).T.flatten(),
+        #                         np.array(individual_performance_data).flatten())
+        mean_performance_data.append(np.mean(individual_performances))
+        line_mean.set_data(range(1, gen_i + 1), mean_performance_data)
+        median_performance_data.append(np.median(individual_performances))
+        line_median.set_data(range(1, gen_i + 1), median_performance_data)
+
+        plt.pause(0.1)  # Pause to update the plot
+        return line,
 
     while criteria_method[config.end_criteria].check(state):
         # Parent Selection
@@ -210,8 +185,11 @@ if __name__ == "__main__":
         print("BEST OF LAST: ", best_of_last)
         print("Best of Last performance: ", best_of_last.performance)
         print(sum(best_of_last.points))
-        state.gen_i += 1
 
+        state.gen_i += 1
+        update_plot(state.gen_i, state)
+
+    plt.show()
     # print("LAST GENERATION: ", generations[-1])
     best_of_last = max(state.generations[-1], key=lambda x: x.performance)
 
---
+++ src/config.py
@@ -0,0 +1,93 @@
+from src.classes import Class
+from src.crossover import (single_point_crossover,
+                           double_point_crossover,
+                           ring_crossover,
+                           uniform_crossover)
+from src.selection import (
+    elite_selection,
+    roulette_selection,
+    boltzmann_selection,
+    universal_selection,
+    tournament_prob,
+    ranking,
+    tournament_det)
+from src.mutation import multi_gene_mutation, gene_mutation
+from src.replacement import traditional_replacement, youth_favoured_replacement
+
+import json
+from collections import defaultdict
+
+
+class Config:
+    def __init__(self, config_file):
+        # Read the JSON configuration from file
+        with open(config_file, 'r') as f:
+            config = json.load(f)
+
+        # Assign configuration parameters to class attributes
+        self.population_size = config["population_size"]
+        self.population_class = config["population_class"]
+        self.child_rate = config["child_rate"]
+        self.mutation_rate = config["mutation_rate"]
+        self.mutation_delta = config["mutation_delta"]
+        self.mutation_method = config["mutation_method"]
+        self.gene_to_modify = config["gene_to_modify"]
+        self.uniform_mutation = config["uniform_mutation"]
+        self.max_generations = config["max_generations"]
+        self.a = config["A"]
+        self.selection_method_1 = config["selection_method_1"]
+        self.selection_method_2 = config["selection_method_2"]
+        self.b = config["B"]
+        self.replacement_method = config["replacement_method"]
+        self.replacement_selection_method_1 = config["replacement_selection_method_1"]
+        self.replacement_selection_method_2 = config["replacement_selection_method_2"]
+        self.crossover_method = config["crossover_method"]
+        self.boltzmann_temperature_start = config["boltzmann_temperature_start"]
+        self.boltzmann_temperature_end = config["boltzmann_temperature_end"]
+        self.boltzmann_temperature_constant = config["boltzmann_temperature_constant"]
+        self.end_criteria = config["end_criteria"]
+        self.max_generations = config["max_generations"]
+        self.content_criteria_limit = config["content_criteria_limit"]
+        self.content_criteria_delta = config["content_criteria_delta"]
+        self.end_delta = config["end_delta"]
+        self.min_fitness = config["min_fitness"]
+
+
+population_class = defaultdict(lambda: Class.ARCHER)
+population_class.update({
+    'ARCHER': Class.ARCHER,
+    'WARRIOR': Class.WARRIOR,
+    'DEFENDER': Class.DEFENDER,
+    'INFILTRATOR': Class.INFILTRATOR,
+})
+
+selection_methods = defaultdict(lambda: elite_selection)
+selection_methods.update({
+    'elite': elite_selection,
+    'roulette': roulette_selection,
+    'universal': universal_selection,  # TODO
+    'boltzmann': boltzmann_selection,
+    'tournament_det': tournament_det,
+    'tournament_prob': tournament_prob,
+    'ranking': ranking     # TODO
+})
+
+crossover_methods = defaultdict(lambda: single_point_crossover)
+crossover_methods.update({
+    'single_point': single_point_crossover,
+    'double_point': double_point_crossover,
+    'ring': ring_crossover,
+    'uniform': uniform_crossover
+})
+
+replacement_method = defaultdict(lambda: youth_favoured_replacement)
+replacement_method.update({
+    'youth_favoured': youth_favoured_replacement,
+    'traditional': traditional_replacement
+})
+
+mutation_method = defaultdict(lambda: gene_mutation)
+mutation_method.update({
+    'gene': gene_mutation,
+    'multi_gene': multi_gene_mutation
+})
